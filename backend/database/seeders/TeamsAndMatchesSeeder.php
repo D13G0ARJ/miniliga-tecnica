a1751f7ee9b9548fcdb9e7d932f3f72cbe6dbe64
D13G0ARJ <rodrijime34@gmail.com>
2025-11-01 13:54:54 -0400

    feat/implement-match-result-endpoint

--- backend/database/seeders/TeamsAndMatchesSeeder.php
+++ backend/database/seeders/TeamsAndMatchesSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace Database\Seeders;
+
+namespace Database\Seeders; 
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -12,13 +13,14 @@ class TeamsAndMatchesSeeder extends Seeder
      */
     public function run(): void {
     $teams = collect(['Dragons','Sharks','Tigers','Wolves'])
-      ->map(fn($n)=>App\Models\Team::create(['name'=>$n]));
+    
+      ->map(fn($n)=>\App\Models\Team::create(['name'=>$n]));
 
     // crea 2-3 partidos sin resultado
-    App\Models\Game::create([
+    \App\Models\Game::create([
       'home_team_id'=>$teams[0]->id, 'away_team_id'=>$teams[1]->id
     ]);
-    App\Models\Game::create([
+    \App\Models\Game::create([
       'home_team_id'=>$teams[2]->id, 'away_team_id'=>$teams[3]->id
     ]);
 }
